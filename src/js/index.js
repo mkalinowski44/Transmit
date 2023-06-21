c670104a93a78e84bed275c8cd36ae6ef6d03000
Marcin Kalinowski <marcin36k@outlook.com>
2023-06-21 19:52:47 +0200

    Cookies translated

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,12 @@ navigation()
 home()
 
 CookiesInfo({
+  title: 'Cookies Policy',
   infoButton: {
-     link: 'domain.com/cookiesinfo',
+     link: 'https://cookieinformation.com/what-is-a-cookie/',
      blank: true,
+     text: 'Find out more'
   },
+  acceptText: 'Accept',
+  description: 'This website uses cookies. By using the website, you agree to the storage or reading of cookies according to your browser settings.'
 })
