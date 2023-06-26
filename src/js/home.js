ec0313927286ef8f2d71e266e22c2f5b6aede69c
Marcin Kalinowski <marcin36k@outlook.com>
2023-06-26 17:15:40 +0200

    Update brands sizes

--- src/js/home.js
+++ src/js/home.js
@@ -5,12 +5,31 @@ export default function home_init() {
   $('.brands .slick-slider').slick({
     dots: false,
     arrows: false,
-    centerMode: true,
-    variableWidth: true,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 2000,
     speed: 500,
+    slidesToShow: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        }
+      }
+    ]
   })
 
   const collapseButtons = $('.collapse-button')
